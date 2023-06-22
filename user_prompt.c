53fe988f1269348a8ef8cae2cd1222fceb472042
patmens <nanay5077@gmail.com>
2023-06-22 09:22:35 -0700

    user_prompt file

---
+++ user_prompt.c
@@ -0,0 +1,16 @@
+include "header.h"
+
+/**
+ * prompt_function-This is tpo display a prompt interface to the user and reads input
+ * @ac: arguement counter, counts the number of 
+ *         command-line arguements passed to the program.
+ * @argv: an array used to access the values of the command-line arguements passed.
+ * shell_env: Executable enviroment
+ * return: returns 0 on failure.
+ */
+
+int main(int ac, char **argv, char **shell_env)
+	if(ac = = 1)
+	prompt_function(argv , shell_env)
+	return(0);
+
