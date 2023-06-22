7c77497ce888ee3ed736eb81b525d1e49ad19015
patmens <nanay5077@gmail.com>
2023-06-22 11:50:59 -0700

    prompt function file

--- prompt_function.c
+++ prompt_function.c
@@ -5,19 +5,19 @@
   * Thi is the Prompt_function
   */
 
-void user_prompt(char **argv, char **shell_env)
+void prompt_function(char **av, char **shell_env)
 {
 char *string = NULL;
-int i , status;
+int i, status;
 size_t n = 0;
 ssize_t numb;
-char *argv[] = {NULL , NULL};
+char *argv[] = {NULL, NULL};
 pid_t child_pid;
 
 while(1)
     {
-	    printf("userpromt_interface$   ");
-	    numb = getline($string, $n, stdin);
+	    printf("userpromt_interface$ ");
+	    numb = getline(&string, &n, stdin);
 	    if(numb == -1)
 	    {
              free(string);
@@ -26,7 +26,7 @@ while(1)
 	    i =0;
 	    while(string[i])
 	    {
-            if(sting[i] = = "\n");
+            if(sting[i] == '\n');
 	    string[i] = 0;
 	    i++;
 	    }
@@ -34,18 +34,19 @@ while(1)
 	    argv[0] = string;
 	    child_pid = fork();
 	    
-	    if(child_pid = = -1)
+	    if(child_pid == -1)
 	    {
             free(string);
 	    exit(EXIT_FAILURE);
 	    }
 
-	    if(child_pid = = 0)
+	    if(child_pid == 0)
 	    {
-             if(execve (argv[0] , argv , shell_env) = = -1)
+             if(execve(argv[0], argv, shell_env) == -1)
+		     printf("%s: No such file or directory exit,sorry\n", av[0]);
 	    }
 	    else 
-	    {
+	   
 		    wait(status);
 	    }
     }
