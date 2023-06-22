0d5870e6385849a9815d2613182e40e70100c646
patmens <nanay5077@gmail.com>
2023-06-22 10:00:48 -0700

    header file updated

--- header.h
+++ header.h
@@ -1,8 +1,21 @@
-#ifndef HEADER_H
-#define HEADER_H
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+  * Checks function
+  */
 
 int _abs(int c)
 
+/**
+  * User Prompt function
+  */
+
+void prompt_function(char **argv , char **shell_env)
 
 
 
