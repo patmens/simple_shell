8360c52f88a8872ec849bacf6f2c24e8d0007055
Mary <meditation708@gmail.com>
2023-06-22 16:26:04 -0700

    prompt

--- prompt_function.c
+++ prompt_function.c
@@ -2,52 +2,54 @@
 #include <sys/wait.h>
 
 /**
-  * Thi is the Prompt_function
-  */
+* prompt_function- displays a prompt interface to the user and reads input
+* command-line arguements passed to the program.
+* @arv: Array used to access the values of command-line arguements passed.
+* @shell_env: Executable enviroment
+* Return: returns 0 on failure.
+*/
 
-void prompt_function(char **av, char **shell_env)
+void prompt_function(char **arv, char **shell_env)
 {
-char *string = NULL;
-int i, status;
-size_t n = 0;
-ssize_t numb;
-char *argv[] = {NULL, NULL};
-pid_t child_pid;
+	char *line = NULL;
+	int i;
+	int status;
+	size_t n = 0;
+	ssize_t numb;
+	char *argv[2];
+	pid_t child_pid;
 
-while(1)
-    {
-	    printf("userpromt_interface$ ");
-	    numb = getline(&string, &n, stdin);
-	    if(numb == -1)
-	    {
-             free(string);
-	     exit(EXIT_FAILURE);
-	    }
-	    i =0;
-	    while(string[i])
-	    {
-            if(string[i] == '\n');
-	    string[i] = 0;
-	    i++;
-	    }
+	while (1)
+	{
+		printf("userprompt_interface$"); /*The shell prompt*/
+		numb = getline(&line, &n, stdin);
+		if (numb == -1)
+		{
+			free(line);
+			exit(EXIT_FAILURE);
+		}
+		i = 0;
+		while (line[i])
+		{
+			if (line[i] == '\n')
+			line[i] = 0;
+			i++;
+		}
+		argv[0] = line;
+		argv[1] = NULL;
+		child_pid = fork();
 
-	    argv[0] = string;
-	    child_pid = fork();
-	    
-	    if(child_pid == -1)
-	    {
-            free(string);
-	    exit(EXIT_FAILURE);
-	    }
-
-	    if(child_pid == 0)
-	    {
-             if(execve(argv[0], argv, shell_env) == -1)
-		     printf("%s: No such file or directory exit,sorry\n", av[0]);
-	    }
-	    else 
-	   
-		    wait(status);
-	    }
-    }
+		if (child_pid == -1)
+		{
+			free(line);
+			exit(EXIT_FAILURE);
+		}
+		if (child_pid == 0)
+		{
+			if (execve(argv[0], arv, shell_env) == -1)
+			printf("%s: No such file or directory exit,sorry\n", arv[0]);
+		}
+		else
+		wait(&status);
+	}
 }
--- user_prompt.c
+++ user_prompt.c
@@ -1,18 +1,17 @@
 #include "header.h"
 
 /**
- * prompt_function-This is tpo display a prompt interface to the user and reads input
- * @ac: arguement counter, counts the number of 
- *         command-line arguements passed to the program.
- * @argv: an array used to access the values of the command-line arguements passed.
- * shell_env: Executable enviroment
- * return: returns 0 on failure.
+ * main- displays a prompt interface to the user and reads input
+ * @ac: arguement counter, counts the number of
+ * command-line arguements passed to the program.
+ * @*arv: array used to access the values of command-line arguements passed.
+ * @shell_env: Executable enviroment
+ * Return: returns 0 on failure.
  */
 
-int main(int ac, char **av, char **shell_env)
+int main(int ac, char **arv, char **shell_env)
 {
-	if(ac == 1)
-	prompt_function(av, shell_env);
-
-	return(0);
+	if (ac == 1)
+	prompt_function(arv, shell_env);
+	return (0);
 }
